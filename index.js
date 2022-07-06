8820b3a0eb8ce36f66272b61961e3f987fe3e0a6
CHARLYODIN <81874130+CHARLYODIN@users.noreply.github.com>
2022-07-05 21:44:11 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
-{
-const miTitulo = "Proyectos CHARLYODIN";
-const h = document.querySelector('h1');
-h.textContent = miTitulo;
-}
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
 
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+  }
+});
