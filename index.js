a954b9d87761fb060d79b7d4db94046edcb2639d
CHARLYODIN <81874130+CHARLYODIN@users.noreply.github.com>
2022-07-12 22:23:10 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,37 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
 });
+
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel__lista'), {
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   dots: '.carousel__indicador',
+   arrows: {
+     prev: '.carousel__anterior',
+     next: '.carousel__siguiente'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: '2',
+        slidesToScroll: '2',
+        itemWidth: 150,
+        duration: 0.25
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        itemWidth: 150,
+        duration: 0.25
+      }
+    }
+  ]
+  });
+});
